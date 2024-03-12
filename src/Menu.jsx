046119ebcb4bcbf0ef13ea9ceeaae31b5c5ff209
Tomas Zeman <zemant24@gmail.com>
2024-03-12 17:43:53 +0100

    Add app menu

---
+++ src/Menu.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+const Menu = () => {
+  // State for is nav expanded
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
+
+  // Toggle state
+  const toggleNav = () => {
+    setIsNavExpanded(!isNavExpanded);
+  };
+
+  // Closed menu
+  const closeNav = () => {
+    setIsNavExpanded(false);
+  };
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark" style={{ width: '100%' }}>
+      <div className="container-fluid" style={{ maxWidth: '960px', margin: '0 auto' }}>
+        <NavLink className="navbar-brand" to="/">Clustering Application</NavLink>
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={toggleNav}
+          aria-controls="navbarNav"
+          aria-expanded={isNavExpanded}
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className={`collapse navbar-collapse ${isNavExpanded ? 'show' : ''}`} id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink className="nav-link" activeClassName="active" to="/" onClick={closeNav}>Clustering</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" activeClassName="active" to="/todo-1" onClick={closeNav}>Todo</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" activeClassName="active" to="/todo-2" onClick={closeNav}>Todo</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Menu;
