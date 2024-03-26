7d17d485ebf797bdbf6c9bf5509879170d882f15
Tomas Zeman <zemant24@gmail.com>
2024-03-26 14:52:41 +0100

    Preparing methods for article abstract

--- src/ClusterChart.jsx
+++ src/ClusterChart.jsx
@@ -3,23 +3,35 @@ import { Scatter } from 'react-chartjs-2';
 import 'chart.js/auto';
 import { options } from './ChartOptions';
 
-const ClusterChart = ({ data, selectedCluster }) => {
+const ClusterChart = ({ data, selectedCluster, onPointClick }) => {
   const chartData = {
     datasets: [{
       label: 'Cluster Points',
       data: data.filter(item => item.cluster === Number(selectedCluster)).map(item => ({
         x: item.x,
-        y: item.y
+        y: item.y,
+        articleTitle: item.filename,
       })),
       backgroundColor: 'rgba(54, 162, 235, 1)',
     }]
   };
 
+  // Extend options with event handler
+  const extendedOptions = {
+    ...options,
+    onClick: (event, elements) => {
+      if (elements.length > 0) {
+        const elementIndex = elements[0].index;
+        const datasetIndex = elements[0].datasetIndex;
+        const title = chartData.datasets[datasetIndex].data[elementIndex].articleTitle;
+        onPointClick(title);
+      }
+    },
+  };
+
   return (
-    <div className="d-flex justify-content-center w-100 my-3">
-      <div style={{ width: '600px', height: '400px' }}>
-        <Scatter data={chartData} options={options} />
-      </div>
+    <div class="cluster-chart">
+      <Scatter data={chartData} options={extendedOptions} />
     </div>
   );
 };
--- src/ClusterDetail.jsx
+++ src/ClusterDetail.jsx
@@ -1,25 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import getClusterDetail from './GetClusterDetail';
 import ClusterSelect from './ClusterSelect';
 import ClusterChart from './ClusterChart';
+import '../styles/cluster-detail.css'
 
 const ClusterDetail = () => {
   const apiUrl = `${import.meta.env.VITE_APP_API_URL}/get-data`;
   const { data, clusters, selectedCluster, setSelectedCluster } = getClusterDetail(apiUrl);
+  const [selectedArticleTitle, setSelectedArticleTitle] = useState('');
 
   return (
-    <div className="d-flex flex-column align-items-center">
-      {clusters.length > 0 && (
-        <ClusterSelect
-          clusters={clusters}
-          selectedCluster={selectedCluster}
-          setSelectedCluster={setSelectedCluster}
-        />
-      )}
+    <div className="container mt-3">
+      <div className="row justify-content-center">
+        <div className="col-auto">
+          {clusters.length > 0 && (
+            <ClusterSelect
+              clusters={clusters}
+              selectedCluster={selectedCluster}
+              setSelectedCluster={setSelectedCluster}
+            />
+          )}
 
-      {selectedCluster && (
-        <ClusterChart data={data} selectedCluster={selectedCluster} />
-      )}
+          <div className="d-flex">
+            <div className="me-3">
+              {selectedCluster && (
+                <ClusterChart
+                  data={data}
+                  selectedCluster={selectedCluster}
+                  onPointClick={setSelectedArticleTitle}
+                />
+              )}
+            </div>
+            <div className="align-self-start abstract-container">
+              {selectedArticleTitle && (
+                <>
+                  <h4>Abstract</h4>
+                  <p>{selectedArticleTitle}</p>
+                </>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
