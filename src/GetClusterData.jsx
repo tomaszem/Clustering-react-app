437e9e3c5b43209e6a04b999c3c4f1b9b1754476
Tomas Zeman <zemant24@gmail.com>
2024-04-05 22:31:10 +0200

    Update GetCluster API

--- src/GetClusterData.jsx
+++ src/GetClusterData.jsx
@@ -9,12 +9,24 @@ const getClusterData = (apiUrl) => {
             .then(data => {
                 const clusters = {};
                 data.forEach(point => {
-                    const { cluster, x, y } = point;
-                    if (!clusters[cluster]) {
-                        clusters[cluster] = { x: [], y: [], type: 'scatter', mode: 'markers', name: `Cluster ${cluster}`, marker: { size: 12 }};
+                    const { initial_cluster, x_3d, y_3d, z_3d, filename } = point;
+                    if (!clusters[initial_cluster]) {
+                        clusters[initial_cluster] = {
+                            x: [],
+                            y: [],
+                            z: [],
+                            type: 'scatter3d',
+                            mode: 'markers',
+                            name: `Cluster ${initial_cluster}`,
+                            marker: { size: 5 },
+                            text: [],
+                            hoverinfo: 'text',
+                        };
                     }
-                    clusters[cluster].x.push(x);
-                    clusters[cluster].y.push(y);
+                    clusters[initial_cluster].x.push(x_3d);
+                    clusters[initial_cluster].y.push(y_3d);
+                    clusters[initial_cluster].z.push(z_3d);
+                    clusters[initial_cluster].text.push(`${filename}`);
                 });
 
                 setData(Object.values(clusters));
