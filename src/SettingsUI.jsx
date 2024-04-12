2f6c757bbd0ed9724f296c4107d23081ac748d2f
Tomas Zeman <zemant24@gmail.com>
2024-04-12 13:13:18 +0200

    Create new left menu in settings

--- src/SettingsUI.jsx
+++ src/SettingsUI.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import EpsRangeDisplay from "./EpsRange";
 import EpsForm from "./EpsForm";
+import CronTimer from "./CronTimer";
 
 const SettingsUI = () => {
+  const [activeComponent, setActiveComponent] = useState("CronTimer");
+
+  const displayComponent = (componentName) => {
+    setActiveComponent(componentName);
+  };
+
   return (
-    <div>
-      <h2>Settings</h2>
-      <EpsRangeDisplay />
-      <EpsForm />
+    <div className="settings-container">
+      <div className="settings-menu">
+        <h3>Menu</h3>
+        <ul>
+          <li onClick={() => displayComponent("CronTimer")}>Cron Timer</li>
+          <li onClick={() => displayComponent("EpsRangeDisplay")}>EPS Range Display</li>
+          <li onClick={() => displayComponent("EpsForm")}>EPS Form</li>
+        </ul>
+      </div>
+      <div className="settings-components">
+        {activeComponent === "CronTimer" && <CronTimer />}
+        {activeComponent === "EpsRangeDisplay" && <EpsRangeDisplay />}
+        {activeComponent === "EpsForm" && <EpsForm />}
+      </div>
     </div>
   );
 };
