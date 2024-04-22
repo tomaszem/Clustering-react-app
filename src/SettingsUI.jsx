c16b048f65350dc177b3a69af4501fc644aff55a
Tomas Zeman <zemant24@gmail.com>
2024-04-22 09:08:23 +0200

    Add latest components into settings

--- src/SettingsUI.jsx
+++ src/SettingsUI.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import EpsRangeDisplay from "./EpsRange";
 import EpsForm from "./EpsForm";
 import CronTimer from "./CronTimer";
+import SetClustersNum from './SetClustersNum'
+import SetCronTime from "./SetCronTime";
 
 const SettingsUI = () => {
   const [activeComponent, setActiveComponent] = useState("CronTimer");
@@ -15,15 +17,19 @@ const SettingsUI = () => {
       <div className="settings-menu">
         <h3>Menu</h3>
         <ul>
-          <li onClick={() => displayComponent("CronTimer")}>Cron Timer</li>
+          <li onClick={() => displayComponent("CronTimer")}>View Current Cron Timer</li>
+          <li onClick={() => displayComponent("SetCronTime")}>Set Cron Time</li>
           <li onClick={() => displayComponent("EpsRangeDisplay")}>EPS Range Display</li>
-          <li onClick={() => displayComponent("EpsForm")}>EPS Form</li>
+          <li onClick={() => displayComponent("EpsForm")}>Set EPS Value</li>
+          <li onClick={() => displayComponent("SetClustersNum")}>Set Number Of Clusters</li>
         </ul>
       </div>
       <div className="settings-components">
         {activeComponent === "CronTimer" && <CronTimer />}
+        {activeComponent === "SetCronTime" && <SetCronTime />}
         {activeComponent === "EpsRangeDisplay" && <EpsRangeDisplay />}
         {activeComponent === "EpsForm" && <EpsForm />}
+        {activeComponent === "SetClustersNum" && <SetClustersNum />}
       </div>
     </div>
   );
